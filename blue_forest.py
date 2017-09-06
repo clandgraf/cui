e8bd10d9da481cca670fe416c62839381295ee98
Christoph Landgraf <christoph.landgraf@gmail.com>
2017-09-06 23:49:12 +0100

    Theme file

---
+++ blue_forest.py
@@ -0,0 +1,14 @@
+from cui.api import *
+
+def_colors('white',      '#e8dfd7')
+def_colors('black',      '#203439')
+def_colors('light_grey', '#506064')
+def_colors('dark_grey',  '#304247')
+def_colors('magenta',    '#fc5a7b')
+
+def_foreground('modeline_active',   'magenta')
+def_background('modeline_active',   'light_grey')
+def_foreground('modeline_inactive', 'magenta')
+def_background('modeline_inactive', 'dark_grey')
+def_foreground('selection',         None)
+def_background('selection',         'light_grey')
