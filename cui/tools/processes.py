bd17ed0c181fa528833ce7bb7e6905218e111f9b
Christoph Landgraf <christoph.landgraf@gmail.com>
2018-01-29 18:12:03 +0100

    Fixed processes.py

--- cui/tools/processes.py
+++ cui/tools/processes.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import cui
 import os
 import select
 import signal
@@ -19,9 +20,8 @@ class Process(object):
         self._proc = None
 
     def start(self):
-        self._pread, self._pwrite = os.pipe()
-        self._proc = subprocess.Popen(self._args, stdout=self._pwrite, stdin=self._pread)
-        cui.register_waitable(self._pread, self.handle)
+        self._proc = subprocess.Popen(self._args, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=0)
+        cui.register_waitable(self._proc.stdout, self.handle)
 
     def stop(self):
         self.kill()
@@ -29,13 +29,15 @@ class Process(object):
     def kill(self):
         os.kill(self._proc.pid, signal.SIGINT)
 
-    def handle(self):
-        self.handle_input(os.read(self._pread, ProcessPipe.BUFFER_SIZE))
+    def handle(self, pread):
+        self.handle_input(self._proc.stdout.read(Process.BUFFER_SIZE))
 
     def handle_input(self):
+        # Overwrite in subclass
         pass
 
 
 class LineBufferedProcess(tools.LineReader, Process):
     def handle_line(self, line):
+        # Overwrite in subclass
         pass
