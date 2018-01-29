47fe69bd3371a93a0f7f71a9abc34adf9c2e32f9
Christoph Landgraf <christoph.landgraf@gmail.com>
2018-01-29 18:13:16 +0100

    Included cui-gdb in metrics.sh

--- metrics.sh
+++ metrics.sh
@@ -1,1 +1,1 @@
-bin/metrics `find ./cui src/cui-emacs/cui_emacs src/cui-pydevd/cui_pydevd src/cui-source/cui_source -type f -name "*.py"`
+bin/metrics `find ./cui src/cui-emacs/cui_emacs src/cui-pydevd/cui_pydevd src/cui-source/cui_source src/cui-gdb/cui_gdb -type f -name "*.py"`
