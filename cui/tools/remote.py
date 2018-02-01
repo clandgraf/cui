588da9555b4db6a59015afa89a80897fac4e5f67
Christoph Landgraf <christoph.landgraf@gmail.com>
2018-02-01 18:02:26 +0100

    Updated Headers

---
+++ cui/tools/remote.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2017-2018 Christoph Landgraf. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from .server import Session, Server
+
+class RemoteSession(object):
+    pass
